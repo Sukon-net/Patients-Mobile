8ae8c6f53e41f78fcde481f67777ce11f11e30c8
Nadin Ahmed <nadinahmed316@gmail.com>
2024-12-06 15:48:52 +0200

    add text styles

---
+++ lib/core/theme/text_styles.dart
@@ -0,0 +1,57 @@
+import 'dart:ui';
+
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class TextStyles{
+  TextStyles._();
+
+  static TextStyle size30Weight700 = TextStyle(
+    fontSize: 30.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle size18Weight600 = TextStyle(
+    fontSize: 18.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle size16Weight500 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w500,
+  );
+
+  static TextStyle size14Weight400 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle size16Weight700 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w700,
+  );
+
+  static TextStyle size24Weight600 = TextStyle(
+    fontSize: 24.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle size16Weight400 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w400,
+  );
+
+  static TextStyle size16Weight600 = TextStyle(
+    fontSize: 16.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle size14Weight600 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w600,
+  );
+
+  static TextStyle size14Weight500 = TextStyle(
+    fontSize: 14.sp,
+    fontWeight: FontWeight.w500,
+  );
+}
