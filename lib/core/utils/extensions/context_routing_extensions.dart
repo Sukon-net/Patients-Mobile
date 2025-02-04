837af44633b664ed4cb33a67bd98325468ac5022
Nadin Ahmed <nadinahmed316@gmail.com>
2025-02-05 00:30:15 +0200

    Update Navigation extensions

--- lib/core/utils/extensions/context_routing_extensions.dart
+++ lib/core/utils/extensions/context_routing_extensions.dart
@@ -1,20 +1,23 @@
+import 'package:clients/core/routing/navigator_service.dart';
 import 'package:flutter/material.dart';
 
 extension ContextRoutingExtensions on BuildContext {
   Future<dynamic> pushNamed(String routeName, {Object? arguments}) {
-    return Navigator.of(this).pushNamed(routeName, arguments: arguments);
+    return NavigatorService.navigatorKey.currentState!
+        .pushNamed(routeName, arguments: arguments);
   }
 
   Future<dynamic> pushReplacementNamed(String routeName, {Object? arguments}) {
-    return Navigator.of(this)
+    return NavigatorService.navigatorKey.currentState!
         .pushReplacementNamed(routeName, arguments: arguments);
   }
 
   Future<dynamic> pushNamedAndRemoveUntil(String routeName,
       {Object? arguments, required RoutePredicate predicate}) {
-    return Navigator.of(this)
+    return NavigatorService.navigatorKey.currentState!
         .pushNamedAndRemoveUntil(routeName, predicate, arguments: arguments);
   }
 
-  void pop({dynamic returnValue}) => Navigator.of(this).pop(returnValue);
+  void pop({dynamic returnValue}) =>
+      NavigatorService.navigatorKey.currentState!.pop(returnValue);
 }
